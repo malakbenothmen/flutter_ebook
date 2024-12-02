9b6a3cffed2436cd77262e53d3070a98700d3ca7
malakbenothmen <malekbenothmen70@gmail.com>
2024-12-02 14:46:06 +0100

    user interfaces ok

---
+++ android/app/src/main/kotlin/com/example/ebook_prj/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ebook_prj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:ebook_prj/pages/root_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: RootApp(),
+  ));
+}
+
+
